184c47162273f1fb14663c693b82a539a4825827
akrischer <krischer.a@husky.neu.edu>
2016-10-25 01:53:49 -0400

    Add base events

---
+++ Grid/Assets/scripts/BaseEvent.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class BaseEvent : MonoBehaviour
+{
+
+    private event EventHandler Triggered;
+    private event EventHandler Finished;
+
+    // Use this for initialization
+    protected void Start()
+    {
+        AddOnTrigger(OnTrigger);
+        AddOnTrigger(OnFinish);
+    }
+
+
+    public void TestTrigger()
+    {
+        Triggered(this, EventArgs.Empty);
+    }
+
+    #region Event Boilerplate
+
+    public void AddOnTrigger(EventHandler eh)
+    {
+        Triggered += eh;
+    }
+
+    public void RemoveOnTrigger(EventHandler eh)
+    {
+        Triggered -= eh;
+    }
+
+    public void AddOnFinished(EventHandler eh)
+    {
+        Finished += eh;
+    }
+
+    public void RemoveOnFinished(EventHandler eh)
+    {
+        Finished -= eh;
+    }
+
+    #endregion
+
+    public virtual void OnTrigger(object sender, EventArgs e)
+    {
+        Debug.Log("ON TRIGGER called with sender \"" + sender + "\", and event args \"" + e + "\"");
+    }
+
+    public virtual void OnFinish(object sender, EventArgs e)
+    {
+        Debug.Log("ON FINISH called with sender \"" + sender + "\", and event args \"" + e + "\"");
+    }
+}
---
+++ Grid/Assets/scripts/MainQuestEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class MainQuestEvent : BaseEvent {
+
+    public override void OnTrigger(object sender, EventArgs e)
+    {
+        Debug.Log("Starting main quest");   
+    }
+
+    public override void OnFinish(object sender, EventArgs e)
+    {
+        Debug.Log("Finished main quest");
+    }
+}
