4af7bb6d05ca2933be78dcd15b7622e20ce177a5
akrischer <krischer.a@husky.neu.edu>
2016-10-31 16:06:41 -0400

    update base event description

--- Grid/Assets/scripts/Events/BaseEvent.cs
+++ Grid/Assets/scripts/Events/BaseEvent.cs
@@ -1,10 +1,26 @@
 ﻿using UnityEngine;
 using System;
-using System.Collections;
+using System.Collections.Generic;
 
+/// <summary>
+/// A base event describes a base in-game event that can be triggered. BaseEvents also have the ability to trigger an OnFinish event.
+/// 
+/// To use:
+///     Let's say I want to open a window when this event is triggered.
+///     
+///     I have some WindowManager.cs script which has some function 'OpenNewWindow(object, EventArgs)' that opens a new window
+///     
+///     In WindowManager.cs, I need to subscribe this method I defined to the event that I want to trigger the window opening.
+///     Let's call this event 'myEvent,' and let's assume we're currently in the WindowManager.cs script file.
+///     
+///     To subscribe the method to the event, just call
+///         myEvent.AddOnTrigger(OpenNewWindow)
+///         
+///     Most likely, you want to call this on script initialization (e.g. Awake/Start). What this does is tells the event to call the method
+///     'OpenNewWindow' whenever the 'Triggered' event is called.
+/// </summary>
 public class BaseEvent : MonoBehaviour
 {
-
     private event EventHandler Triggered;
     private event EventHandler Finished;
 
