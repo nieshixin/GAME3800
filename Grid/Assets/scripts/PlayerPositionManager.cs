8579625918e971dc7500b01f413b619a629d9e44
akrischer <krischer.a@husky.neu.edu>
2016-10-31 15:33:53 -0400

    add player movement

--- Grid/Assets/scripts/PlayerPositionManager.cs
+++ Grid/Assets/scripts/PlayerPositionManager.cs
@@ -18,24 +18,40 @@ public class PlayerPositionManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		GameObject[] tiles = GameObject.FindGameObjectsWithTag ("Tile");
+
+		foreach (GameObject  tile in tiles) {
+			if (tile.transform.position == player.transform.position) {
+				tile.GetComponent<BoxCollider2D> ().enabled = false;
+			}
+			if (tile.transform.position != player.transform.position) {
+				tile.GetComponent<BoxCollider2D> ().enabled = true;
+			}
+		}
+
+
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
 			if (CanMove (MOVE_DIRECTION.UP)) {
 				// move player up
+				player.GetComponent<player>().moveUp();
 				Debug.Log("Player moved up");
 			}
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			if (CanMove (MOVE_DIRECTION.DOWN)) {
 				// move player down
+				player.GetComponent<player>().moveDown();
 				Debug.Log("Player moved down");
-			}
+			}	
 		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 			if (CanMove (MOVE_DIRECTION.LEFT)) {
 				// move player down
+				player.GetComponent<player>().moveL();
 				Debug.Log("Player moved left");
 			}
 		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
 			if (CanMove (MOVE_DIRECTION.RIGHT)) {
 				// move player down
+				player.GetComponent<player>().moveR();;
 				Debug.Log("Player moved right");
 			}
 		}
@@ -53,28 +69,29 @@ public class PlayerPositionManager : MonoBehaviour {
 		// has right?
 		RaycastHit2D hit = Physics2D.Raycast(player.transform.position, Vector2.right);
 
-		if (hit.collider != null && (Vector2.Distance(player.transform.position, hit.collider.transform.position) <= 1)) {
+		if (hit.collider != null && (Vector2.Distance(player.transform.position, hit.collider.transform.position) <= 1) && hit.collider.transform.position != player.transform.position) {
 			// then we know there is a tile here
 			result.Add(MOVE_DIRECTION.RIGHT, true);
 		}
 
 		// has left?
 		hit = Physics2D.Raycast(player.transform.position, Vector2.left);
-		if (hit.collider != null && (Vector2.Distance(player.transform.position, hit.collider.transform.position) <= 1)) {
+		if (hit.collider != null && (Vector2.Distance(player.transform.position, hit.collider.transform.position) <= 1) && hit.collider.transform.position != player.transform.position) {
 			// then we know there is a tile here
 			result.Add(MOVE_DIRECTION.LEFT, true);
 		}
 
 		// has up?
 		hit = Physics2D.Raycast(player.transform.position, Vector2.up);
-		if (hit.collider != null && (Vector2.Distance(player.transform.position, hit.collider.transform.position) <= 1)) {
+		if (hit.collider != null && (Vector2.Distance(player.transform.position, hit.collider.transform.position) <= 1) && hit.collider.transform.position != player.transform.position) {
 			// then we know there is a tile here
 			result.Add(MOVE_DIRECTION.UP, true);
+
 		}
 
 		// has down?
 		hit = Physics2D.Raycast(player.transform.position, Vector2.down);
-		if (hit.collider != null && (Vector2.Distance(player.transform.position, hit.collider.transform.position) <= 1)) {
+		if (hit.collider != null && (Vector2.Distance(player.transform.position, hit.collider.transform.position) <= 1) && hit.collider.transform.position != player.transform.position) {
 			// then we know there is a tile here
 			result.Add(MOVE_DIRECTION.DOWN, true);
 		}
--- Grid/Assets/scripts/player.cs
+++ Grid/Assets/scripts/player.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class player : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	public void moveUp() {
+		transform.Translate (Vector2.up * 1);
+
+	}
+	public void moveR() {
+		transform.Translate (Vector2.right * 1);
+
+	}
+	public void moveL() {
+		transform.Translate (Vector2.left * 1);
+
+	}
+	public void moveDown() {
+		transform.Translate (Vector2.down * 1);
+
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
