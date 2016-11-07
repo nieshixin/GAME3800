421c1f5d3a9372c73ee0c410fcf567d87080bd89
akrischer <krischer.a@husky.neu.edu>
2016-11-07 15:54:22 -0500

    Add managers to scene

---
+++ Grid/Assets/scripts/Managers/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    private static Text _location;
+    private static Image _backgroundImage;
+    private static Image _playerPanelBackgroundImage;
+    private static Image _npcPanelBackgroundImage;
+
+	public static Text Location
+    {
+        get
+        {
+            return _location;
+        }
+        set
+        {
+            _location = value;
+        }
+    }
+
+    public static Image BackgroundImage
+    {
+        get
+        {
+            return _backgroundImage;
+        }
+        set
+        {
+            _backgroundImage = value;
+        }
+    }
+
+    public static Image PlayerPanelBackgroundImage
+    {
+        get
+        {
+            return _playerPanelBackgroundImage;
+        }
+        set
+        {
+            _playerPanelBackgroundImage = value;
+        }
+    }
+
+    void Awake()
+    {
+        _location = GameObject.FindGameObjectWithTag(Tags.LOCATION_TEXT_UI).GetComponent<Text>();
+        _backgroundImage = GameObject.FindGameObjectWithTag(Tags.BACKGROUND_IMAGE_UI).GetComponent<Image>();
+        _playerPanelBackgroundImage = GameObject.FindGameObjectWithTag(Tags.PLAYER_PANEL_BACKGROUND_IMAGE_UI).GetComponent<Image>();
+    }
+}
--- Grid/Assets/scripts/Util/Tags.cs
+++ Grid/Assets/scripts/Util/Tags.cs
@@ -7,4 +7,8 @@ public class Tags {
     public static readonly string PLAYER = "Player";
     public static readonly string EVENT_DESCRIPTION_UI = "EventDescription_UI";
     public static readonly string TILE = "Tile";
+
+    public static readonly string LOCATION_TEXT_UI = "LocationTextUI";
+    public static readonly string BACKGROUND_IMAGE_UI = "BackgroundImageUI";
+    public static readonly string PLAYER_PANEL_BACKGROUND_IMAGE_UI = "PlayerPanelBackgroundImageUI";
 }
