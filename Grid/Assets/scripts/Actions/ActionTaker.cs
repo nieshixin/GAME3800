fc2b76caf96e61392ad7db4207810e894fcbe2e4
DESKTOP-CGE94BR\Jerry Nie <nieshixin@gmail.com>
2016-11-13 15:48:43 -0500

    action list and receiving actions

---
+++ Grid/Assets/scripts/Actions/ActionTaker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionTaker : MonoBehaviour {
+	public enum AVAILABLE_ACTIONS{
+		laugh, cry, mock
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
