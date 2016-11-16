9e625fc0306ebc4e3b98ab07eb11c3cc662f51c7
Chao Fang <fang.chao1@northeastern.edu>
2016-11-16 16:30:40 -0500

    Refactor base enemy behavior into its own script

---
+++ Grid/Assets/scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyBase : MonoBehaviour {
+
+    //all action effects takeplace in enemy scripts, each enemy can have different reactions towards actions
+    //below is the list of actions that enemy can respond:
+    public List<ActionTaker.AVAILABLE_ACTIONS> myReactionList = new List<ActionTaker.AVAILABLE_ACTIONS>();
+
+    //helper function to check when a text input was received, if it was in the actionlist also in the enemy's reaction list.
+    internal bool CanTakeAction(string PlayerTextInput)
+    {
+        if ((Enum.IsDefined(typeof(ActionTaker.AVAILABLE_ACTIONS), PlayerTextInput)) &&
+            myReactionList.Contains(((ActionTaker.AVAILABLE_ACTIONS)(Enum.Parse(typeof(ActionTaker.AVAILABLE_ACTIONS), PlayerTextInput)))))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Grid/Assets/scripts/Enemies/enemyExample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class enemyExample : EnemyBase {
+
+	// Use this for initialization
+	void Start () {
+		//so from all available actions: laugh, cry, mock, this enemy accept 2 of them
+		myReactionList.Add(ActionTaker.AVAILABLE_ACTIONS.cry);
+		myReactionList.Add(ActionTaker.AVAILABLE_ACTIONS.laugh);
+	}
+
+
+}
