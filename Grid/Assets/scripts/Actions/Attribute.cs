54d91bd21a8795357e25e2898afbead45cb93918
Chao Fang <fang.chao1@northeastern.edu>
2016-11-16 15:59:34 -0500

    Edit map to look like how David wants it; Add attribute struct to project

---
+++ Grid/Assets/scripts/Actions/Attribute.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public struct Attribute {
+
+    public string name;
+    public string description;
+    public int maxValue;
+    public int currentValue;
+    public string incrementResponse;   // what is returned when the value is incremented
+    public string decrementResponse;   // what is returned when the value decremented
+
+    public bool Is(float val)
+    {
+        return currentValue == val;
+    }
+
+    public string Increment()
+    {
+        currentValue = Mathf.Min(maxValue, currentValue + 1);
+        return incrementResponse;
+    }
+
+    public string Decrement()
+    {
+        currentValue = Mathf.Max(0, currentValue - 1);
+        return decrementResponse;
+    }
+}
