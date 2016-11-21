9b3f7f2f208b8b65c3710fa22158dda06f6e9e83
Jerry Nie <nieshixin@gmail.com>
2016-11-21 15:31:25 -0500

    input field tweak

---
+++ Grid/Assets/scripts/PlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+
+	public void SendPlayerInput(string input){
+		input = GetComponent<InputField>().text;
+		Debug.Log (input);
+	}
+
+	public void ClearInputBar(){
+		GetComponent<InputField> ().text = "";
+
+
+	}
+}
