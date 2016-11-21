81528003d4ddfa54b001a65367d1c1a75c7e6d76
Jerry Nie <nieshixin@gmail.com>
2016-11-21 16:45:00 -0500

    Description Display

---
+++ Grid/Assets/scripts/ShowDescription.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ShowDescription : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.J)) {
+			Debug.Log ("addDescription");
+			AddNewDescription (" - I'm feeling really good high af ahhhhhh");
+		}
+	}
+		
+
+	public void AddNewDescription(string description){
+		GetComponent<Text>().text += System.Environment.NewLine;
+		GetComponent<Text>().text += description;
+	}
+}
